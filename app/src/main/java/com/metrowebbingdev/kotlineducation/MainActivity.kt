ab6e2f343c488dac16a14ef3a3f582ea14e7793d
Anton Finko <metrowebbing@gmail.com>
2023-01-07 13:54:36 +0300

    Step-1

--- app/src/main/java/com/metrowebbingdev/kotlineducation/MainActivity.kt
+++ app/src/main/java/com/metrowebbingdev/kotlineducation/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.metrowebbingdev.kotlineducation.navigation.SetupNavHost
 import com.metrowebbingdev.kotlineducation.ui.theme.KotlineducationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +19,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KotlineducationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val navController = rememberNavController()
+                SetupNavHost(navController = navController)
+               
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    KotlineducationTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/metrowebbingdev/kotlineducation/screens/SplashScreen.kt
@@ -0,0 +1,67 @@
+package com.metrowebbingdev.kotlineducation.screens
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.metrowebbingdev.kotlineducation.navigation.Screens
+import com.metrowebbingdev.kotlineducation.ui.theme.KotlineducationTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+
+    var startAnimated by remember {
+        mutableStateOf(false)
+    }
+    val alphaAnimation = animateFloatAsState(
+        targetValue = if(startAnimated) 1f else 0f,
+        animationSpec = tween(durationMillis = 3000)
+    )
+    LaunchedEffect(key1 = true) {
+        startAnimated = true
+        delay(4000)
+        navController.navigate(Screens.Main.route)
+    }
+    Splash(alpha = alphaAnimation.value)
+
+}
+
+@Composable
+fun Splash(alpha: Float) {
+    Box(modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            modifier = Modifier
+                .size(120.dp).alpha(alpha = alpha),
+            imageVector = Icons.Default.PlayArrow,
+            contentDescription = "",
+            tint = Color.Black
+        )
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun prevSplash() {
+    KotlineducationTheme {
+        Splash(1f)
+
+    }
+}
